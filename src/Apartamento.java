fa0de1e47c97f43eafeb020110d057fefd9f4238
Jucyara Silva <41348701+jucyara@users.noreply.github.com>
2021-03-09 16:48:47 -0300

    IMPLEMENTAÇÃO DO DAO

--- src/Apartamento.java
+++ src/Apartamento.java
@@ -1,7 +1,11 @@
 
-public class Apartamento implements java.io.Serializable {
+import java.util.List;
+
+@Entity
+public class Apartamento {
 	
 	private int bloco;
+	@Id
 	private int numero;
 	
 	public Apartamento() {
@@ -28,5 +32,35 @@ public class Apartamento implements java.io.Serializable {
 	
 	public void setNumero(int numero) {
 		this.numero=numero;
+	}
+	
+	public interface ApartamentoDAO {
+		
+		void salva(Apartamento ap);
+		List<Apartamento> lista();
+	}
+	
+	public class JPAApartamentoDAO implements ApartamentoDAO {
+		
+		public void salva(Apartamento ap) {
+			EntityManager em =  abreConexao();
+			em.getTransaction().begin();
+			
+			em.persist(ap);
+			
+			em.getTransaction().commit();
+			em.close();
+		}
+		
+		public List<Apartamento> lista() {
+			EntityManager em = abreConexao();
+			
+			List<Apartamento> apartamento = em.createQuery("select a from Apartamento ap").getResultList();
+			
+			em.close();
+			
+			return apartamento;
+		}
+		
 	}
 }
