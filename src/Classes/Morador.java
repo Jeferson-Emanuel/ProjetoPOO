748b4e30cc1564acc0a115562766707411fd4253
Jucyara Silva <41348701+jucyara@users.noreply.github.com>
2021-03-28 20:32:48 -0300

    Tirei só o comentário //NÃO CONSEGUI para não confundir o professor

--- src/Classes/Morador.java
+++ src/Classes/Morador.java
@@ -8,7 +8,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Column;
 import javax.persistence.Table;
 
-@Entity //NAO CONSEGUI
+@Entity
 @Table(name="morador")
 public class Morador {
 	
